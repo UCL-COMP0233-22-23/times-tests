0c42ae4ba5e59b87131050ed495b3f77e82a22e5
Sheila <zcahsmp@ucl.ac.uk>
2022-11-03 10:45:50 +0000

    ValueError for inverted time intervals

--- test_times.py
+++ test_times.py
@@ -1,4 +1,5 @@
 from times import compute_overlap_time, time_range
+from pytest import raises
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -27,3 +28,9 @@ def test_end_start():
     result = compute_overlap_time(range1, range2)
     expected = [('0000-00-00 00:00:00', '0000-00-00 00:00:00')]
     assert result == expected
+
+def test_fails_inverted_interval():
+    with raises(ValueError):
+        range1 = time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
+        range2 = time_range("2010-01-12 12:00:00", "2010-01-12 13:45:00",2, 60)
+        compute_overlap_time(range1, range2)
