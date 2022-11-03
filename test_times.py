94ace23e0030dfd5ab497e06dd8cf9a4b5338751
OscarCharlieST <ucapocs@ucl.ac.uk>
2022-11-03 11:07:23 +0000

    Added input test which raises a value error
    Answers UCL-COMP0233-22-23/RSE-Classwork#20

--- test_times.py
+++ test_times.py
@@ -2,7 +2,7 @@
 
 from times import compute_overlap_time
 from times import time_range
-import datetime
+import pytest
 
 def test_subset_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2)
@@ -34,5 +34,11 @@ def test_zero_overlap():
     result = compute_overlap_time(large, short)    
     expected = 0.0
     assert result == expected
+    
+def test_input_values():
+    with pytest.raises(ValueError):
+        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2)
+        short = time_range("2010-01-12 10:50:00", "2010-01-12 10:45:00", 2)
+        result = compute_overlap_time(large, short)
 
 # Forgot to mention Answers UCL-COMP0233-22-23/RSE-Classwork#19
--- times.py
+++ times.py
@@ -1,8 +1,13 @@
 import datetime
 import time
+from pytest import raises
 
 
-def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
+def correctly_ordered_inputs(range0):
+    if range0[0][0] > range0[-1][1]:
+        raise ValueError("\n The input contains a time range which goes backwards in time as index is increased. \n Please ensure inputs are time-ordered.")
+
+def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):  
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
@@ -12,7 +17,13 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
+
 def compute_overlap_time(range1, range2):
+    
+    # Checking inputs
+    correctly_ordered_inputs(range1)
+    correctly_ordered_inputs(range2)
+    
     latest_start = max(min([start for start, end in range1]), 
                        min([start for start, end in range2]))
     earliest_end = min(max([end for start, end in range1]), 
@@ -23,9 +34,10 @@ def compute_overlap_time(range1, range2):
         return ee_sec - ls_sec
     else:
         return 0
+    
 
 if __name__ == "__main__":
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 10:31:00", 2)
+    large = time_range("2010-01-12 10:35:00", "2010-01-12 10:31:00", 2)
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:50:00", 2)
     print()
     print(compute_overlap_time(large, short), 'seconds of overlap')
