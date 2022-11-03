76e540550c6a4060f375b5f175b7c7253cae2a90
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-11-03 10:45:14 +0000

    multiple time tests

--- test_times.py
+++ test_times.py
@@ -10,3 +10,32 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
+
+def test_given_input_2():
+
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
+
+    result = compute_overlap_time(large, short)
+    expected = []
+    assert result == expected
+
+def test_given_input_3():
+
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",3,120)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+    result = compute_overlap_time(large, short)
+    expected =[('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:38:40'), ('2010-01-12 10:40:40', '2010-01-12 10:45:00')]
+    assert result == expected
+
+
+def test_given_input_4():
+
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 14:00:00")
+
+    result = compute_overlap_time(large, short)
+    expected = []
+    assert result == expected
+
