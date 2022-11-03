a2abd659c4a440b6cb0ee7fcd786ae96408efdee
pkruzikova <p.kruziku@gmail.com>
2022-11-03 11:14:48 +0000

    Automating tests

--- times.py
+++ times.py
@@ -20,7 +20,7 @@ def compute_overlap_time(range1, range2):
             overlap_time.append((low, high))
     return overlap_time
 
-if __name__ == "__main__":
+if __name__ == "__main__": #this will only be executed when file is run directly, not imported
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
