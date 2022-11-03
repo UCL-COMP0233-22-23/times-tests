82abc948f26bd76a3d39c5b796fc732f5e9e63a7
Angus Mingare <ucapafm@ucl.ac.uk>
2022-11-03 21:19:44 +0000

    Parameterised tests Answers UCL-COMP0233-22-23/RSE-Classwork#27

--- test_times.py
+++ test_times.py
@@ -1,31 +1,11 @@
 from times import * 
+import pytest
 from pytest import raises
 
-def test_given_input():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large,short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+@pytest.mark.parametrize("range1, range2, expected", [(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]), (time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00"), time_range("2100-01-01 10:00:00", "2101-01-01 10:00:00"), []), (time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00", 2, 60), time_range("2000-06-01 10:00:00", "2001-06-01 10:00:00", 2, 120),[('2000-06-01 10:00:00', '2000-07-02 09:59:30'), ('2000-07-02 10:00:30', '2000-11-30 21:59:00'), ('2000-11-30 22:01:00', '2001-01-01 10:00:00')]), (time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00") , time_range("2001-01-01 10:00:00", "2002-01-01 10:00:00"), [("2001-01-01 10:00:00", "2001-01-01 10:00:00")])])
+def test_given_input(range1, range2, expected):
+    result = compute_overlap_time(range1,range2)
     assert result == expected
-
-def test_given_input_2():
-    range1 = time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00")
-    range2 = time_range("2100-01-01 10:00:00", "2101-01-01 10:00:00")
-    result = compute_overlap_time(range1,range2)
-    expected = []
-    assert result == expected
-
-def test_given_input_3():
-    range1 = time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00", 2, 60)
-    range2 = time_range("2000-06-01 10:00:00", "2001-06-01 10:00:00", 2, 120)
-    result = compute_overlap_time(range1,range2)
-    assert len(result) == 3
-
-def test_given_input_4():
-    range1 = time_range("2000-01-01 10:00:00", "2001-01-01 10:00:00") 
-    range2 = time_range("2001-01-01 10:00:00", "2002-01-01 10:00:00")
-    result = compute_overlap_time(range1,range2)
-    assert result == [("2001-01-01 10:00:00", "2001-01-01 10:00:00")]
 
 def test_time_range_invalid_input():
     with raises(ValueError):
