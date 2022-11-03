46890cbf85627e975065e90b580032762147504e
Anda-Raluca Epure <anda.epure.20@ucl.ac.uk>
2022-11-03 10:18:53 +0000

    Wrote 3 additional tests for the compute_overlap_time method.

--- test_times.py
+++ test_times.py
@@ -7,3 +7,25 @@ def test_given_input():
     result = times.compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_several_intervals_empty_intersection():
+
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = times.time_range("2011-01-12 10:30:00", "2011-01-12 10:45:00", 2, 60)
+    result = times.compute_overlap_time(large, short)
+    expected = []
+    assert result == expected
+
+def test_several_intervals_nonempty_intersections():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 12, 15)
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = times.compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:03', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:39:50'), ('2010-01-12 10:40:05', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_several_intervals_one_second_intersections():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00", 12, 15)
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = times.compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:30:00')]
+    assert result == expected
