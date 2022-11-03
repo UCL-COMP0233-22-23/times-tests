17d7fe4859f66c77fc078e8858ac341cfd10b7a2
Sheila <zcahsmp@ucl.ac.uk>
2022-11-03 10:18:38 +0000

    Test passed removing first if statement

--- times.py
+++ times.py
@@ -15,12 +15,10 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            if end1 > start2:
-                low = max(start1, start2)
-                high = min(end1, end2)
-                if low < high:
-                    overlap_time.append((low, high))
-            
+            low = max(start1, start2)
+            high = min(end1, end2)
+            if low < high:
+                overlap_time.append((low, high))
     if overlap_time == []:
         overlap_time = [('0000-00-00 00:00:00', '0000-00-00 00:00:00')]
     return overlap_time
