0da537ccf4af6975e78059232236d70db2f431a5
harveymannering <harveymannering@hotmail.com>
2022-11-03 10:47:30 +0000

    Added value error

--- test_times.py
+++ test_times.py
@@ -1,4 +1,6 @@
 from times import time_range, compute_overlap_time
+from pytest import raises
+
 """create a test each in test_times.py for:
 
  - two time ranges that do not overlap
@@ -14,15 +16,19 @@ def test_several_intervals():
     intervals_1 = time_range("2010-01-12 10:30:00", "2010-01-12 11:30:00", 2, 60) 
     intervals_2 = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00", 2, 60) 
     print(compute_overlap_time(intervals_1, intervals_2))
-    #assert compute_overlap_time(intervals_1, intervals_2) == [('2010-01-12 11:00:30', '2010-01-12 11:29:30')]
+    assert compute_overlap_time(intervals_1, intervals_2) == [('2010-01-12 11:00:30', '2010-01-12 11:29:30')]
 
 def test_perfect_overlap():
     intervals_1 = time_range("2010-01-12 10:30:00", "2010-01-12 11:30:00", 1, 0) 
     intervals_2 = time_range("2010-01-12 10:30:00", "2010-01-12 11:30:00", 1, 0) 
     assert compute_overlap_time(intervals_1, intervals_2) == [('2010-01-12 10:30:00', '2010-01-12 11:30:00')]
 
+def test_invalid_inputs():
+    with raises(ValueError):
+        time_range("2010-01-12 10:30:00", "2010-01-12 09:30:00", 1, 0)
 
 if __name__ == "__main__":
     test_given_input()
-    test_several_intervals()
+    #test_several_intervals()
+    test_invalid_inputs()
 
