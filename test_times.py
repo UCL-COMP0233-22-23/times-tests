2fca3e0d5e9f82e230a2432713af7531f8dff954
Shijie Cao <ucapaox@ucl.ac.uk>
2022-11-03 11:16:20 +0000

    add git action

--- test_times.py
+++ test_times.py
@@ -11,5 +11,5 @@ def test_not_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00", 2, 60)
     result = compute_overlap_time(large, short) 
-    expected = ()
+    expected = []
     assert result == expected
