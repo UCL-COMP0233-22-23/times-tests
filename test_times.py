d426c0e155bacd07126003048f6e26a4ff9025bb
JingyaoLIz <ucapliz@ucl.ac.uk>
2022-11-03 10:23:03 +0000

    multiple tests

--- test_times.py
+++ test_times.py
@@ -1,12 +1,49 @@
 from times import time_range,compute_overlap_time
 
 def test_given_input(): 
-    if __name__ == "__main__":
-        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-        print(compute_overlap_time(large, short))
+    
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(compute_overlap_time(large, short))
     
     result = []
     result = compute_overlap_time(large, short)
     expected  = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+    
+
+    
+    #two time ranges that do not overlap
+    large = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00")
+    print(compute_overlap_time(large, short))
+    
+    result1 = []
+    result1 = compute_overlap_time(large, short)
+    expected1  = []
+    assert result1 == expected1
+    
+
+    
+    #two time ranges that both contain several intervals each
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(compute_overlap_time(large, short))
+    
+    result2 = []
+    result2 = compute_overlap_time(large, short)
+    expected2  = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:40:00')]
+    assert result2 == expected2
+    
+
+    
+    #two time ranges that end exactly at the same time when the other starts
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(compute_overlap_time(large, short))
+    
+    result3 = []
+    result3 = compute_overlap_time(large, short)
+    expected3  = []
+    assert result3 == expected3
+    
--- times.py
+++ times.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
@@ -20,6 +19,8 @@ def compute_overlap_time(range1, range2):
             overlap_time.append((low, high))
     return overlap_time
 
-large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+'''
+large = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00")
+short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00")
 print(compute_overlap_time(large, short))
+'''
