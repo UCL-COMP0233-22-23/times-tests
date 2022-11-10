55375b36e90d026c92304195d5611f57446debbd
iynix <ucaphud@ucl.ac.uk>
2022-11-10 21:00:36 +0000

    to test github connection

--- times.py
+++ times.py
@@ -1,13 +1,22 @@
 import datetime
-
+import pytest
+#to test if github connection is alright
+def value_error_warning(start_time_s, end_time_s):
+    if start_time_s>end_time_s:
+        raise ValueError("Input time {},{} is invalid".format(start_time_s,end_time_s))
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    with python.raise(ValueError):
+        
+        #start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+        #end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
+                start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
+                for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
