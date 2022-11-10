9dd955620bf301eea752a235d833e51ef8cbe9cd
YOU ZUO <zuoyou1998@gmail.com>
2022-11-10 14:25:42 +0000

    Answers UCL-COMP0233-22-23/RSE-Classwork#27

--- times.py
+++ times.py
@@ -1,11 +1,9 @@
 import datetime
-import pytest
+
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
-    if start_time_s >= end_time_s:
-        raise ValueError("backwards")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
@@ -17,9 +15,11 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            if low < high:
+            # both ranges need to start before the other ends, otherwise there is no overlap!
+            if start1 <= end2 and start2 <= end1: 
+                low = max(start1, start2)
+                high = min(end1, end2)
+                if high == low:
+                    continue
                 overlap_time.append((low, high))
     return overlap_time
-
